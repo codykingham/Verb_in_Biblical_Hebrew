4c0b7896da8b2eb1f87c595502af7a1f6659f698
Cody Kingham <codyakingham@gmail.com>
2017-07-24 23:15:14 +0200

    add new tables/statistics in notebook 2

---
+++ functions/verbs.py
@@ -0,0 +1,32 @@
+
+# pull in TF methods
+from __main__ import F, T, L, E
+
+def is_weqt(verb_node):
+    '''
+    Validate whether a given verb_node (word_node in TF) is a weqetal.
+    Only the qatal, or perfect is stored in the ETCBC database.
+    Typicaly the weqetal is set apart by being in discourse
+        with an attached waw.
+    '''
+
+    tense = F.vt.v(verb_node)
+    clause = L.u(verb_node, otype='clause')[0]
+
+    # check if tense is perfect
+    if tense != 'perf':
+        return False
+	
+    # begin looking for features of weqetal
+    # get clause domain
+    domain = F.domain.v(clause)
+    
+    # look for attached waw in preceding word
+    lex_before = L.u(verb_node-1, otype='lex')[0] # lex obj of preceding
+    attached_waw = True if F.lex.v(lex_before) == 'W' else False
+    
+    # weqetal if there is a waw anddomain is discursive/quotation
+    if attached_waw and domain in {'D', 'Q'}:
+        return True
+    else:
+        return False
